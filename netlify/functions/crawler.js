2a51554bd22da37e2aadbfbc6e5f2679b7b4fde3
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2021-06-09 17:03:07 -0400

    Updated response format netlify function

--- netlify/functions/crawler.js
+++ netlify/functions/crawler.js
@@ -30,7 +30,7 @@ exports.handler = async (event, context) => {
     
     return { 
         statusCode: 200, 
-        body: JSON.stringify({ products: data.products }) 
+        body: JSON.stringify(data.products) 
     };
   } catch (error) {
     console.log(error);
