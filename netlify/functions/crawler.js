bde599f6e9dd188d69c5073324c6d1cc7a6b4328
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2021-06-09 16:37:18 -0400

    Refact netlify functions

--- netlify/functions/crawler.js
+++ netlify/functions/crawler.js
@@ -1,28 +1,28 @@
+const gql = require('graphql-tag');
+const { ApolloClient, createHttpLink, InMemoryCache } = require('@apollo/client/core');
+
+const cache = new InMemoryCache()
+
+const httpLink = createHttpLink({
+    uri: 'https://api-ca-central-1.graphcms.com/v2/ckpn42kbid0wf01xsfl4ehoq0/master',
+})
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+})
+
+const crawlerQuery = {
+    query: gql`query {
+        products {
+          id,
+          title,
+          price
+        }
+      }`
+}
+
 exports.handler = async (event, context) => {
-  import gql from 'graphql-tag'
-  import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
-
-  const cache = new InMemoryCache()
-
-  const httpLink = createHttpLink({
-      uri: 'https://api-ca-central-1.graphcms.com/v2/ckpn42kbid0wf01xsfl4ehoq0/master',
-  })
-
-  const apolloClient = new ApolloClient({
-      link: httpLink,
-      cache,
-  })
-
-  const crawlerQuery = {
-      query: gql`query {
-          products {
-            id,
-            title,
-            price
-          }
-        }`
-  }
-
   try {
     const { data } = await apolloClient.defaultClient.query(crawlerQuery);
     
