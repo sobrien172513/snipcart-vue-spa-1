011507080023c157dc67ca1cf3203c9353f0533d
Steve O'Brien <steveobrien73@gmail.com>
2022-12-11 00:24:57 -0500

    edit hygraph api src

--- src/main.js
+++ src/main.js
@@ -1,43 +1,45 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApp } from "vue";
+import App from "./App.vue";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
 //import { createApolloProvider } from "@vue/apollo-option"
-import VueApollo from '@vue/apollo-option'
-import ProductComponent from './components/ProductComponent'
-import ProductsComponent from './components/ProductsComponent'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import VueApollo from "@vue/apollo-option";
+import ProductComponent from "./components/ProductComponent";
+import ProductsComponent from "./components/ProductsComponent";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const httpLink = createHttpLink({
-    uri: 'https://api-us-east-1.graphcms.com/v2/ckxt3ez5w2gg701z3apj8cgws/master',
-})
+  // https://api-us-east-1.graphcms.com/v2/ckxt3ez5w2gg701z3apj8cgws/master
+  uri: "https://api-us-east-1.hygraph.com/v2/ckxt3ez5w2gg701z3apj8cgws/master",
+});
 
 // Cache implementation
-const cache = new InMemoryCache()
+const cache = new InMemoryCache();
 
 // Create the apollo client
 const apolloClient = new ApolloClient({
-    link: httpLink,
-    cache,
-})
+  link: httpLink,
+  cache,
+});
 
 // const apolloProvider = createApolloProvider({
 //   defaultClient: apolloClient,
 // })
 const apolloProvider = new VueApollo({
-    defaultClient: apolloClient,
-})
-  
+  defaultClient: apolloClient,
+});
+
 const routes = [
-    { path: '/', component: ProductsComponent },
-    { path: '/product/:id', component: ProductComponent },
-]
+  { path: "/", component: ProductsComponent },
+  { path: "/product/:id", component: ProductComponent },
+];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes: routes
-})
+  history: createWebHashHistory(),
+  routes: routes,
+});
 
-createApp(App)
-    .use(router)
-    .provide('apollo', apolloProvider)
-    .mount('#app')
+createApp(App).use(router).provide("apollo", apolloProvider).mount("#app");
